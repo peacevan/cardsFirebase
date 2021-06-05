c8a1363afc43380c4220c0352d1284a56689eee0
peacevan <peacevan@hotmail.com>
2021-06-04 21:21:21 -0300

    Update main.js

--- agenda/agenda/js/main.js
+++ agenda/agenda/js/main.js
@@ -3,12 +3,12 @@
 
 // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyCnfKe3-cOwfjd6PRhl70BPskZVCgaOW6A",
+    apiKey: "",
     authDomain: "atividadeunifacs.firebaseapp.com",
     databaseURL: "https://atividadeunifacs.firebaseio.com",
     projectId: "atividadeunifacs",
     storageBucket: "gs://atividadeunifacs.appspot.com/",
-    messagingSenderId: "863353273173"
+    messagingSenderId: ""
   };
   firebase.initializeApp(config);
   
